884f17b1c6cfd946b77f5b22ba5adefcc0b095ce
Max Gagner <m.gagner@hotmail.se>
2025-01-02 17:27:07 +0100

    started project

---
+++ vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navigation from './components/Navigation.vue'
+import HeroImage from './components/HeroImage.vue'
+</script>
+
+<template>
+  <header>
+    <Navigation />
+  </header>
+
+  <HeroImage />
+  
+  <main>
+    <TheWelcome />
+  </main>
+</template>
