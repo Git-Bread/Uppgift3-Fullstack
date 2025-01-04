1e3d52123bea55eace3d6c0a817ccd043440d6c6
Max Gagner <m.gagner@hotmail.se>
2025-01-04 01:21:39 +0100

    fixed preview, only api connection left

--- vue-project/src/views/GameList.vue
+++ vue-project/src/views/GameList.vue
@@ -1,5 +1,5 @@
 <script>
-import {ref} from "vue";
+import {ref, watch} from "vue";
 import DataRow from "../components/DataRow.vue";
 let url="http://127.0.0.1:8000/api/games"
 
@@ -10,6 +10,11 @@ export default {
     },
     setup() {
         let ObjectList = ref(["empty"]);
+        let gameName = ref("");
+        let gameType = ref("");
+        let gameRating = ref("");
+        let gameRelease = ref("");
+
         async function run() {
             try {
                 let newData = await getData();
@@ -38,8 +43,20 @@ export default {
 
         run();
 
+        watch(gameRating, (value) => {
+            if (value < 0) gameRating.value = 0;
+            else if (value > 5) gameRating.value = 5;
+            else if (value % 0.5 != 0) {
+                gameRating.value = 1;
+            }
+        });
+
         return {
-            ObjectList
+            ObjectList,
+            gameName,
+            gameType,
+            gameRating,
+            gameRelease
         };
     }
 }
@@ -68,51 +85,52 @@ export default {
             </div>
         </section>
 
-        <section class="p-6 bg-slate-200 rounded-3xl mt-5 md:mt-0 md:ml-2 ">
+        <section class="p-6 bg-slate-200 rounded-3xl mt-5 md:mt-0 md:ml-2 flex">
             <div class="max-w-lg mx-auto">
                 <h2 class="text-3xl text-center pb-2">Add new Game</h2>
                 
                 <form id="gameForm" class="bg-white p-6 rounded-lg shadow-lg">
                     <div class="mb-4">
                         <label for="gameName" class="block text-sm font-medium text-gray-700">Name</label>
-                        <input
-                        type="text"
-                        id="gameName"
-                        required
-                        class="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        />
+                        <input type="text" v-model="gameName" id="gameName" required class="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"/>
                     </div>
                     <div class="mb-4">
                         <label for="type" class="block text-sm font-medium text-gray-700">Type</label>
-                        <input
-                        type="text"
-                        id="type"
-                        required
-                        class="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        />
+                        <input type="text" v-model="gameType" id="type" required class="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"/>
                     </div>
                     <div class="mb-4">
-                        <label for="rating" class="block text-sm font-medium text-gray-700">Rating</label>
-                        <input
-                        type="text"
-                        id="rating"
-                        required
-                        class="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        />
+                        <label for="rating" class="block text-sm font-medium text-gray-700">Rating (0-5, increments of 0.5)</label>
+                        <input type="number" min="0" max="5" v-model="gameRating" step="0.5" id="rating" require class="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"/>
                     </div>
                     <div class="mb-4">
                         <label for="release" class="block text-sm font-medium text-gray-700">Release</label>
-                        <input
-                        type="text"
-                        id="release"
-                        required
-                        class="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        />
+                        <input type="date" v-model="gameRelease" id="release" required class="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-indigo-500"/>
                     </div>
                     <div class="text-center">
                         <button type="submit" class="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">Submit</button>
                     </div>
                 </form>
+            </div>
+            <div class="min-w-48 p-2 pt-0">
+                <h2 class="text-3xl text-center pb-2">Preview:</h2>
+                <div class="bg-white p-6 rounded-lg shadow-lg max-w-lg mx-auto h-fit">
+                    <div>
+                        <h3>Name:</h3>
+                        <p>{{ gameName }}</p>
+                    </div>
+                    <div>
+                        <h3>Type:</h3>
+                        <p>{{ gameType }}</p>
+                    </div>
+                    <div>
+                        <h3>Rating:</h3>
+                        <p>{{ gameRating }}</p>
+                    </div>
+                    <div>
+                        <h3>Release:</h3>
+                        <p>{{ gameRelease }}</p>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
