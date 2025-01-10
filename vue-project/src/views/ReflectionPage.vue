3180375ccb5431d8f08a12718821442dc29e0a0f
Max Gagner <m.gagner@hotmail.se>
2025-01-10 16:16:01 +0100

    finished

--- vue-project/src/views/ReflectionPage.vue
+++ vue-project/src/views/ReflectionPage.vue
@@ -1,13 +1,22 @@
 <template>
     <section class="p-8 bg-slate-200 max-w-fit m-auto rounded-3xl">
         <h2 class="pb-4 text-3xl">Thoughts on Vue</h2>
-        <div class="flex flex-col md:flex-row md:divide-x-2 md:divide-solid md:divide-slate-700 gap-4">
-            <div class="flex flex-col md:flex-row md:divide-x-2 md:divide-solid md:divide-slate-700 gap-4">
-                <div class="pr-4">
-
+        <div>
+            <div>
+                <div class="">
+                    <p>
+                        Min första åsikt om vue är posetiv, funktioner såsom bindningar och routing gör det enkelt att bygga
+                        responsiva moderna webbplatser. Strukturen tog lite tid att förstå men så fort det satt gick utväcklingen utan större problem.
+                        Det flesta problem gick även fort att lösa tack vare den bra dokumentationen. Gillar den nästan object inriktide strukturen med komponenter,
+                        views och props.
+                    </p>
                 </div>
-                <div class="md:pl-6">
-
+                <div class="pt-4">
+                    <p>
+                        Jag gjorde det dock lite svårare en det behövde vara genom att inkludera tailwind i uppgiften, aldrig hålt på med det förut och det skiljer sig 
+                        en del från vanlig css eller andra ramverk såsom bulma. Tog en stund att få det att fungera väl men är rätt så nöjd med resultatet. Försökte även
+                        hitta en bra bild ifrån Pexels eller liknande "free to use" bilder... men hade ingen tur så ritade något i paint istället.
+                    </p>
                 </div>
             </div>
         </div>
