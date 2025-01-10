97b05d9a612edd73028bb3e693d189522b6ad89e
Max Gagner <m.gagner@hotmail.se>
2025-01-10 12:54:21 +0100

    started reflectons

--- vue-project/src/views/ReflectionPage.vue
+++ vue-project/src/views/ReflectionPage.vue
@@ -1,5 +1,17 @@
 <template>
-    <h1>Ö;AP</h1>
+    <section class="p-8 bg-slate-200 max-w-fit m-auto rounded-3xl">
+        <h2 class="pb-4 text-3xl">Thoughts on Vue</h2>
+        <div class="flex flex-col md:flex-row md:divide-x-2 md:divide-solid md:divide-slate-700 gap-4">
+            <div class="flex flex-col md:flex-row md:divide-x-2 md:divide-solid md:divide-slate-700 gap-4">
+                <div class="pr-4">
+
+                </div>
+                <div class="md:pl-6">
+
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
